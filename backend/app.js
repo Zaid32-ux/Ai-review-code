c2c245d68ce4a6cd36d9930c8cc9fdb21db2d887
Zaid32-ux <Zgour5071@gmail.com>
2026-06-16 19:04:49 +0530

    Add auth and AI routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 import  dbConnection  from "./database/dbConnection.js";
+import authRoutes from "./routes/auth.routes.js";
+import aiRoutes from "./routes/auth.routes.js"
 
 config({ path: "./.env" });
 const app = express();
@@ -8,6 +10,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/auth", authRoutes);
+app.use("/ai", aiRoutes);
 
  dbConnection();
 export default app;
